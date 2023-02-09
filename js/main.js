7b7630deae389c4f0dd3abb3325e55774ccd2029
julianomachadoo <julmachadoramos@gmail.com>
2023-02-09 18:34:29 -0300

    calculo de imc para todos os pacientes

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
-var tdImc = paciente.querySelector(".info-imc");
-
-var pesoEValido = true;
-var alturaEValida = true;
-
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido");
-    pesoEValido = false;
-    tdImc.textContent = "Peso inválido";
-}
-
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida");
-    alturaEValida = false;
-    tdImc.textContent = "Altura inválida";
-}
-
-if (pesoEValido && alturaEValida) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+for (let i = 0; i < pacientes.length; i++) {
+    var tdPeso = pacientes[i].querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = pacientes[i].querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var tdImc = pacientes[i].querySelector(".info-imc");
+    
+    var pesoEValido = true;
+    var alturaEValida = true;
+    
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        pesoEValido = false;
+        tdImc.textContent = "Peso inválido";
+    }
+    
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida");
+        alturaEValida = false;
+        tdImc.textContent = "Altura inválida";
+    }
+    
+    if (pesoEValido && alturaEValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
