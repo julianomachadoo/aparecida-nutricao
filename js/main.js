bd45b055d6fad4156c71308a21034eb4ba75b4d4
julianomachadoo <julmachadoramos@gmail.com>
2023-02-09 17:34:44 -0300

    operadores lógicos

--- js/main.js
+++ js/main.js
@@ -9,4 +9,24 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEValido = true;
+var alturaEValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido");
+    pesoEValido = false;
+    tdImc.textContent = "Peso inválido";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida");
+    alturaEValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if (pesoEValido && alturaEValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
