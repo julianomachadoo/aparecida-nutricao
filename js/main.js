499051de3162c74487ce4d7a5d050f694d62f96e
julianomachadoo <julmachadoramos@gmail.com>
2023-02-14 11:33:10 -0300

    escutando eventos

--- js/main.js
+++ js/main.js
@@ -6,31 +6,34 @@ var pacientes = document.querySelectorAll(".paciente");
 for (let i = 0; i < pacientes.length; i++) {
     var tdPeso = pacientes[i].querySelector(".info-peso");
     var peso = tdPeso.textContent;
-    
+
     var tdAltura = pacientes[i].querySelector(".info-altura");
     var altura = tdAltura.textContent;
-    
+
     var tdImc = pacientes[i].querySelector(".info-imc");
-    
+
     var pesoEValido = true;
     var alturaEValida = true;
-    
+
     if (peso <= 0 || peso >= 1000) {
-        console.log("Peso inválido");
         pesoEValido = false;
         tdImc.textContent = "Peso inválido";
         pacientes[i].classList.add("paciente-invalido");
     }
-    
+
     if (altura <= 0 || altura >= 3.00) {
-        console.log("Altura inválida");
         alturaEValida = false;
         tdImc.textContent = "Altura inválida";
         pacientes[i].classList.add("paciente-invalido");
     }
-    
+
     if (pesoEValido && alturaEValida) {
         var imc = peso / (altura * altura);
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function() {
+    console.log("Cliquei!");
+});
