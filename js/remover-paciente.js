5e4c0496308c35552eaca4fe5278bdb88a968a60
julianomachadoo <julmachadoramos@gmail.com>
2023-02-28 11:33:56 -0300

    animando a remocao de paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,5 +3,13 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function (event) {
-    event.target.parentNode.remove();
+
+    if (event.target.tagName == 'TD') {
+        event.target.parentNode.classList.add("fadeOut");
+        setTimeout(function () {
+            event.target.parentNode.remove();
+        }, 500);
+    }
+
+
 });
